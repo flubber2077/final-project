a4c3b3d2e1edb047f5304dc2bb632f6535f267f1
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-07-10 12:09:30 -0400

    trying to get data from the api

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header.js';
 import HighlightContainer from './Components/HighlightContainer.js';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    fetch('/songs')
+        .then((response) => response.json())
+        .then((json) => {
+            setSongs(json);
+        })
+}, []);
   
   return (
     <div className="App">
