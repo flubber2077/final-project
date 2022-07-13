37a8ee7980390f1b1c0f4314d3209c074ec15dab
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-07-13 09:48:55 -0400

    implementing frontend

--- client/src/Components/MusicianContainer.js
+++ client/src/Components/MusicianContainer.js
@@ -2,15 +2,14 @@ import React from "react";
 import { useSelector } from "react-redux";
 import MusicianInfo from "./MusicianInfo";
 
-export default function MusicianContainer() {
+export default function MusicianContainer(){
 
     let arrOfMusicians = [];
 
     const song = useSelector((state) => state.song.value);
-    const musicians = song[0].musicians
-    console.log(musicians)
 
-    if (musicians !== undefined) {
+    if (song !== undefined) {
+        const musicians = song[0].musicians
         arrOfMusicians = musicians.map(musician => < MusicianInfo key={musician.id} musician={musician} />);
     }
     return (
